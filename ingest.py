035e9b309be701bfc1b57adb0a7dab71684f5374
rui-carvalho-rlabs <rui.carvalho@randylabs.com>
2024-06-14 19:00:28 +0100

    ingestion process and caas api calls as a script

---
+++ ingest.py
@@ -0,0 +1,115 @@
+import requests
+import sys
+import json
+
+audio_manager_url = 'http://localhost:8081/api/v1/audio'
+api_url = 'http://localhost:8080/api/v1/conversation'
+
+def helper():
+    print('COMMANDS')
+    print('')
+    print('Upload a new conversation and start the processing:')
+    print('python3 ingest.py -a [AUDIO_FILE_PATH] -c [CONVERSATION_CONFIG_FILE_PATH]')
+    print('')
+    print('Process a previously uploaded file')
+    print('python3 ingest.py -c [CONVERSATION_CONFIG_FILE_PATH]')
+    print('')
+    print('Check the processing status of a conversation')
+    print('python3 ingest.py -s [CONVERSATION ID]')
+    print('')
+    print('Get the general information of a conversation')
+    print('python3 ingest.py -i [CONVERSATION ID]')
+    print('')
+    print('Get the timeline of a conversation')
+    print('python3 ingest.py -t [CONVERSATION ID]')
+
+def upload_file(file_path):
+    res = 0
+    try:
+        with open(file_path, 'rb') as audio_file:
+            print('Opened file')
+            response = requests.post(audio_manager_url, files={'file': audio_file})
+            print('here')
+            res = response.status_code
+            if response.status_code == 200:
+                print('File uploaded with success!')
+            else:
+                print('Something went wrong when uploading the file.')
+    except Exception as e:
+        print('Something went wrong when uploading the file.')
+        print(e)
+
+        return 0
+
+    return res
+
+def get_info(conv_id: str):
+    try:
+        response = requests.get(f"{api_url}/{conv_id}")
+        print(response.json())
+
+    except Exception as e:
+        print("Failed to fetch status")
+
+def get_status(conv_id: str):
+    try:
+        response = requests.get(f"{api_url}/{conv_id}/status")
+        print(response.json())
+
+    except Exception as e:
+        print("Failed to fetch status")
+
+def get_timeline(conv_id: str):
+    try:
+        response = requests.get(f"{api_url}/{conv_id}/timeline")
+        print(response.json())
+
+    except Exception as e:
+        print("Failed to fetch status")
+
+def process_call(config_path):
+    try:
+        with open(config_path) as config:
+            config_file = json.load(config)
+        response = requests.post(api_url, json = config_file)
+
+        if response.status_code == 200:
+            print('Processing the conversation. You can now find the conversation in the UI.')
+            print('Conversation id: ' + response.json()['conversation_id'])
+        else:
+            print('Call processing went wrong, please check the configuration file and try again.')
+    except Exception as e:
+        print(f"Something went wrong, please check the configuration file and try again.")
+
+def main():
+    if len(sys.argv) == 5:
+        if sys.argv[1] == '-a' and sys.argv[3] == '-c':
+            res = upload_file(sys.argv[2])
+            if res == 200:
+                process_call(sys.argv[4])
+        else:
+            print('Incorrect arguments.')
+            helper()
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == '-c':
+            process_call(sys.argv[2])
+
+        elif sys.argv[1] == '-i':
+            get_info(sys.argv[2])
+
+        elif sys.argv[1] == '-s':
+            get_status(sys.argv[2])
+
+        elif sys.argv[1] == '-t':
+            get_timeline(sys.argv[2])
+        else:
+            print('Incorrect arguments.')
+            helper()
+    elif len(sys.argv) == 2 and sys.argv[1] == '-h':
+        helper()
+    else:
+        print('Incorrect arguments.')
+        helper()
+
+if __name__ == "__main__":
+    main()
